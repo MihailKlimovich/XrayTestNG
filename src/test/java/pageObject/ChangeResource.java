c1f6addeef714df988e5e78679d3d88c8338cf68
Rostislav Orel <rostislav.orel@succraft.com>
2021-11-17 17:55:24 +0300

    Overbooking and price update

--- src/test/java/pageObject/ChangeResource.java
+++ src/test/java/pageObject/ChangeResource.java
@@ -1,10 +1,14 @@
 package pageObject;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.BasePage;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
 public class ChangeResource extends BasePage {
 
@@ -16,22 +20,66 @@ public class ChangeResource extends BasePage {
     By NEW_RESOURCE_FIELD = By.xpath("//label[text()='Resource']/following::input[@type='search']");
     By REMOVE_BUTTON = By.xpath("//label[text()='Resource']/following::button[@title='Remove']");
     By NEXT_BUTTON = By.xpath("//button[@title='Next']");
+    By FINISH_BUTTON = By.xpath("//button[@title='Finish']");
+    By CONFIRMATION_MESSAGE = By.xpath("//flowruntime-display-text-lwc//span//p[text()='Do you wish to overbook?']");
+    By YES_RADIO_BUTTON = By.xpath("//span//input[@value='Yes']");
+    By NO_RADIO_BUTTON = By.xpath("//span//input[@value='No']");
+    By START_DATE_FIELD = By.xpath("//button[@title='Select a date']/parent::lightning-button-icon/parent::div//input[@name='Start_Date_Time']");
+    By END_DATE_FIELD = By.xpath("//button[@title='Select a date']/parent::lightning-button-icon/parent::div//input[@name='End_Date_Time']");
 
 
     @Step("Change resource")
-    public void changeResource(String newResource) throws InterruptedException {
-        //wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@title='accessibility title']")));
-        //wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(0));
+    public String changeResource(String newResource) throws InterruptedException {
         driver.switchTo().frame(0);
         click3(REMOVE_BUTTON);
         click3(NEW_RESOURCE_FIELD);
         writeText(NEW_RESOURCE_FIELD, newResource);
         click3(By.xpath("//li[@data-name='" + newResource + "']"));
         click4(NEXT_BUTTON);
-        Thread.sleep(2000);
-        tab();
-        enter();
-        driver.switchTo().defaultContent();
+        try {
+            if (wait2.until(ExpectedConditions.presenceOfElementLocated(CONFIRMATION_MESSAGE)) != null){
+                String confirmationMessage = readText(CONFIRMATION_MESSAGE);
+                click3(YES_RADIO_BUTTON);
+                click4(NEXT_BUTTON);
+                click4(FINISH_BUTTON);
+                driver.switchTo().defaultContent();
+                return confirmationMessage;
+            }
+        }catch (TimeoutException e) {
+
+            click4(FINISH_BUTTON);
+            driver.switchTo().defaultContent();
+        }
+        String message = "Message not found";
+        return message;
     }
+
+    @Step("Change resource and start/end time")
+    public String changeResourceAndTime(String newResource, String confirm) throws InterruptedException {
+        driver.switchTo().frame(0);
+        click3(REMOVE_BUTTON);
+        click3(NEW_RESOURCE_FIELD);
+        writeText(NEW_RESOURCE_FIELD, newResource);
+        click3(By.xpath("//li[@data-name='" + newResource + "']"));
+        click4(NEXT_BUTTON);
+        try {
+            if (wait2.until(ExpectedConditions.presenceOfElementLocated(CONFIRMATION_MESSAGE)) != null){
+                String confirmationMessage = readText(CONFIRMATION_MESSAGE);
+                click3(By.xpath("//span//input[@value='" + confirm + "']"));
+                click4(NEXT_BUTTON);
+                click4(FINISH_BUTTON);
+                driver.switchTo().defaultContent();
+                return confirmationMessage;
+            }
+        }catch (TimeoutException e) {
+
+            click4(FINISH_BUTTON);
+            driver.switchTo().defaultContent();
+        }
+        String message = "Message not found";
+        return message;
+    }
+
+
 
 }
--- src/test/java/tests/OverbookingAndPriceUpdate.java
+++ src/test/java/tests/OverbookingAndPriceUpdate.java
@@ -3,11 +3,47 @@ package tests;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pageObject.JsonParser2;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class OverbookingAndPriceUpdate extends BaseTest {
+
+    @BeforeClass
+    public void classLevelSetup() {
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--disable-cache");
+        options.addArguments("--disk-cache-size=1");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        //options.addArguments("user-data-dir=/tmp/temp_profile");
+        options.addArguments(" --whitelisted-ips=\"\"");
+        //options.addArguments("--headless", "window-size=1920,1024", "--no-sandbox");
+        driver = new ChromeDriver(options);
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    /*@AfterClass
+    public void teardown() {
+        driver.close();
+        driver.quit();
+        if (driver != null) {
+            driver = null;
+        }
+    }*/
 
     @Test(priority = 1, description = "LogIn")
     @Severity(SeverityLevel.NORMAL)
@@ -15,6 +51,177 @@ public class OverbookingAndPriceUpdate extends BaseTest {
     public void logIn() throws InterruptedException, IOException {
         loginPage.authoriseURL(SFDX, SFDX_AUTH_URL, ORG_USERNAME);
         loginPageForScratchOrg.logInOnScratchOrg2(driver, ORG_URL, ORG_USERNAME, ORG_PASSWORD);
+    }
+
+    @Test(priority = 2, description = "Preconditions")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void preconditions() throws InterruptedException, IOException {
+        user.addPermissionSet(SFDX, "Overbooking_User", ORG_USERNAME);
+        myceQuotes.deleteQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest'", ORG_USERNAME);
+        resource.deleteResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest1", ORG_USERNAME);
+        resource.deleteResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest2", ORG_USERNAME);
+        resource.deleteResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest3", ORG_USERNAME);
+        resource.deleteResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest4", ORG_USERNAME);
+        resource.deleteResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest5", ORG_USERNAME);
+        StringBuilder hotelRecord= hotel.getHotelSFDX(SFDX, "thn__Unique_Id__c='Demo'", ORG_USERNAME);
+        String propertyID = JsonParser2.getFieldValue(hotelRecord.toString(), "Id");
+        String resourceID1 = resource.createResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest1'" +
+                " thn__Hotel__c='" + propertyID + "' thn__Type__c='Meeting Room'", ORG_USERNAME);
+        String resourceID2 = resource.createResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest2'" +
+                " thn__Hotel__c='" + propertyID + "' thn__Type__c='Meeting Room'", ORG_USERNAME);
+        String resourceID3 = resource.createResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest3'" +
+                " thn__Hotel__c='" + propertyID + "' thn__Type__c='Meeting Room'", ORG_USERNAME);
+        String resourceID4 = resource.createResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest4'" +
+                " thn__Hotel__c='" + propertyID + "' thn__Type__c='Meeting Room' thn__is_Shareable__c=true", ORG_USERNAME);
+        String resourceID5 = resource.createResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest5'" +
+                " thn__Hotel__c='" + propertyID + "' thn__Type__c='Meeting Room' thn__is_Shareable__c=true", ORG_USERNAME);
+        resourceGrouping.createResourceGroupingSFDX(SFDX, "thn__Grouped_Resource__c='" + resourceID2 + "'" +
+                " thn__Resource_Group__c='" + resourceID3 + "'", ORG_USERNAME);
+        String quoteID = myceQuotes.createQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest' thn__Pax__c=5" +
+                " thn__Hotel__c='" + propertyID + "' thn__Arrival_Date__c=" + date.generateTodayDate2()
+                + " thn__Departure_Date__c=" + date.generateTodayDate2_plus(0, 2) + "" +
+                " thn__Closed_Status__c='Won'", ORG_USERNAME);
+    }
+
+    @Test(priority = 3, description = "Create a quote meeting room. Click on change resource to assign a different" +
+            " one from the default. Expected result: No Warning (resource isn’t booked yet")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void case1() throws InterruptedException, IOException {
+        String expectedMessage = "Message not found";
+        StringBuilder quoteRecord = myceQuotes.
+                getQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest'", ORG_USERNAME);
+        String myceQuoteID= JsonParser2.getFieldValue(quoteRecord.toString(), "Id");
+        StringBuilder meetingFullDayRecord = product.getProductSFDX(SFDX, "Name='MEETING FULL DAY'", ORG_USERNAME);
+        String meetingFullDayID = JsonParser2.getFieldValue(meetingFullDayRecord.toString(), "Id");
+        String quoteMeetingRoomId = quoteMeetingRoom.createQuoteMeetingRoomSFDX(SFDX, "thn__MYCE_Quote__c='"
+                + myceQuoteID + "' thn__Product__c='" + meetingFullDayID + "'", ORG_USERNAME);
+        StringBuilder resourceRecord1 = resource.
+                getResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest1'", ORG_USERNAME);
+        String resourceId = JsonParser2.getFieldValue(resourceRecord1.toString(), "Id");
+        myceQuotes.goToMyceQuotes();
+        myceQuotes.openMyceQoteRecord("OverbookingChangePriceAutoTest");
+        myceQuotes.openMeetingRooms();
+        quoteMeetingRoom.selectItem("1");
+        quoteMeetingRoom.clickChangeResource();
+        String message = changeResource.changeResource("OverbookingChangePriceAutoTest1");
+        StringBuilder quoteMeetingRoomRecord = quoteMeetingRoom.
+                getQuoteMeetingRoomSFDX(SFDX, "Id='" + quoteMeetingRoomId + "'", ORG_USERNAME);
+        String quoteMeetingRoomResource = JsonParser2.
+                getFieldValue(quoteMeetingRoomRecord.toString(), "thn__Resource__c");
+        Assert.assertEquals(quoteMeetingRoomResource, resourceId);
+        System.out.println(message);
+        Assert.assertEquals(message, expectedMessage);
+    }
+
+    @Test(priority = 4, description = "Create a new quote meeting room and then assign the same resource as the" +
+            " previous record. Expected result: overbooking message asking for confirmation if you have the permission" +
+            " to overbook.")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void case2() throws InterruptedException, IOException {
+        String expectedMessage = "Do you wish to overbook?";
+        StringBuilder quoteRecord = myceQuotes.
+                getQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest'", ORG_USERNAME);
+        String myceQuoteID= JsonParser2.getFieldValue(quoteRecord.toString(), "Id");
+        StringBuilder meetingHalfDayRecord = product.getProductSFDX(SFDX, "Name='MEETING HALF DAY'", ORG_USERNAME);
+        String meetingHalfDayID = JsonParser2.getFieldValue(meetingHalfDayRecord.toString(), "Id");
+        String quoteMeetingRoomId = quoteMeetingRoom.createQuoteMeetingRoomSFDX(SFDX, "thn__MYCE_Quote__c='"
+                + myceQuoteID + "' thn__Product__c='" + meetingHalfDayID + "'", ORG_USERNAME);
+        StringBuilder resourceRecord1 = resource.
+                getResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest1'", ORG_USERNAME);
+        String resourceId = JsonParser2.getFieldValue(resourceRecord1.toString(), "Id");
+        myceQuotes.goToMyceQuotes();
+        myceQuotes.openMyceQoteRecord("OverbookingChangePriceAutoTest");
+        myceQuotes.openMeetingRooms();
+        quoteMeetingRoom.selectItem("2");
+        quoteMeetingRoom.clickChangeResource();
+        String message = changeResource.changeResource("OverbookingChangePriceAutoTest1");
+        System.out.println(message);
+        Assert.assertEquals(message, expectedMessage);
+    }
+
+    @Test(priority = 5, description = "Update Quote meeting room with conditions: Resource != default resource and" +
+            " assigned resource has groupings. Expected result: No overbooking found, changes are saved.")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void case3() throws InterruptedException, IOException {
+        String expectedMessage = "Message not found";
+        StringBuilder resourceRecord1 = resource.
+                getResourceSFDX(SFDX, "Name='OverbookingChangePriceAutoTest2'", ORG_USERNAME);
+        String resourceId = JsonParser2.getFieldValue(resourceRecord1.toString(), "Id");
+        StringBuilder meetingHalfDayRecord = product.getProductSFDX(SFDX, "Name='MEETING HALF DAY'", ORG_USERNAME);
+        String meetingHalfDayID = JsonParser2.getFieldValue(meetingHalfDayRecord.toString(), "Id");
+        StringBuilder quoteRecord = myceQuotes.
+                getQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest'", ORG_USERNAME);
+        String myceQuoteID= JsonParser2.getFieldValue(quoteRecord.toString(), "Id");
+        myceQuotes.goToMyceQuotes();
+        myceQuotes.openMyceQoteRecord("OverbookingChangePriceAutoTest");
+        myceQuotes.openMeetingRooms();
+        quoteMeetingRoom.selectItem("2");
+        quoteMeetingRoom.clickChangeResource();
+        String message = changeResource.changeResource("OverbookingChangePriceAutoTest2");
+        System.out.println(message);
+        StringBuilder quoteMeetingRoomRecord = quoteMeetingRoom.getQuoteMeetingRoomSFDX(SFDX,
+                "thn__MYCE_Quote__c='" + myceQuoteID + "' thn__Product__c='" + meetingHalfDayID + "'",
+                ORG_USERNAME);
+        String quoteMeetingRoom2Resource = JsonParser2.
+                getFieldValue(quoteMeetingRoomRecord.toString(), "thn__Resource__c");
+        Assert.assertEquals(message, expectedMessage);
+        Assert.assertEquals(quoteMeetingRoom2Resource, resourceId);
+    }
+
+    @Test(priority = 6, description = "Create new meeting room then update: Resource = resource where is" +
+            " shareable = true. Expected result: No Warning.")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void case4() throws InterruptedException, IOException {
+        String expectedMessage = "Message not found";
+        StringBuilder meetingFullDayRecord = product.getProductSFDX(SFDX, "Name='MEETING FULL DAY'", ORG_USERNAME);
+        String meetingFullDayID = JsonParser2.getFieldValue(meetingFullDayRecord.toString(), "Id");
+        StringBuilder quoteRecord = myceQuotes.
+                getQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest'", ORG_USERNAME);
+        String myceQuoteID= JsonParser2.getFieldValue(quoteRecord.toString(), "Id");
+        String quoteMeetingRoomId = quoteMeetingRoom.createQuoteMeetingRoomSFDX(SFDX, "thn__MYCE_Quote__c='"
+                + myceQuoteID + "' thn__Product__c='" + meetingFullDayID + "'", ORG_USERNAME);
+        myceQuotes.goToMyceQuotes();
+        myceQuotes.openMyceQoteRecord("OverbookingChangePriceAutoTest");
+        myceQuotes.openMeetingRooms();
+        quoteMeetingRoom.selectItem("3");
+        quoteMeetingRoom.clickChangeResource();
+        String message = changeResource.changeResource("OverbookingChangePriceAutoTest4");
+        Assert.assertEquals(message, expectedMessage);
+    }
+
+    @Test(priority = 7, description = "Create new meeting room then update: Resource = one with shadow from case 3." +
+            " Expected result: Warning for overbooking.")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void case5() throws InterruptedException, IOException {
+        String expectedMessage = "Do you wish to overbook?";
+        StringBuilder meetingHalfDayRecord = product.getProductSFDX(SFDX, "Name='MEETING HALF DAY'", ORG_USERNAME);
+        String meetingHalfDayID = JsonParser2.getFieldValue(meetingHalfDayRecord.toString(), "Id");
+        StringBuilder quoteRecord = myceQuotes.
+                getQuoteSFDX(SFDX, "Name='OverbookingChangePriceAutoTest'", ORG_USERNAME);
+        String myceQuoteID= JsonParser2.getFieldValue(quoteRecord.toString(), "Id");
+        String quoteMeetingRoomId = quoteMeetingRoom.createQuoteMeetingRoomSFDX(SFDX, "thn__MYCE_Quote__c='"
+                + myceQuoteID + "' thn__Product__c='" + meetingHalfDayID + "'", ORG_USERNAME);
+        myceQuotes.goToMyceQuotes();
+        myceQuotes.openMyceQoteRecord("OverbookingChangePriceAutoTest");
+        myceQuotes.openMeetingRooms();
+        quoteMeetingRoom.selectItem("4");
+        quoteMeetingRoom.clickChangeResource();
+        String message = changeResource.changeResource("OverbookingChangePriceAutoTest3");
+        Assert.assertEquals(message, expectedMessage);
+    }
+
+    @Test(priority = 8, description = "Update meeting room and change resource / start and/or end date/times with" +
+            " expected overbooking. Cancel modifications. Expected result: No changes occur on meeting room.")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Overbooking & price update")
+    public void case6() throws InterruptedException, IOException {
+
     }
 
 }
