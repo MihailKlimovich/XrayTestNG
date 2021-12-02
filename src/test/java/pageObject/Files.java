4bcc9c39bcc7b9d12e16e511167fe34895797a0f
Rostislav <rostislav.orel@succraft.com>
2021-12-02 17:33:16 +0300

    Correction-7

--- src/test/java/pageObject/Files.java
+++ src/test/java/pageObject/Files.java
@@ -33,7 +33,7 @@ public class Files extends BasePage {
         while (attempts < 4) {
             try {
                 wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@title='" + fileName + "']")));
-                click2(SHOW_MORE_ACTION);
+                click4(SHOW_MORE_ACTION);
                 click3(DOWNLOAD_BUTTON);
                 Thread.sleep(3000);
                 break;
