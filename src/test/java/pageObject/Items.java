ac5696a2e9beb3dd783d3c1bdeee7f8a439e570d
Rostislav <rostislav.orel@succraft.com>
2022-04-06 17:03:38 +0300

    Release 1.20

--- src/test/java/pageObject/Items.java
+++ src/test/java/pageObject/Items.java
@@ -32,8 +32,8 @@ public class Items extends BasePage {
 
     @Step("Open Items page")
     public Items goToItems() throws InterruptedException {
-        //driver.navigate().to("https://thynk-test-unlocked-dev-ed.lightning.force.com/lightning/o/thn__Item__c/list?filterName=Recent");
-        driver.navigate().to("https://thautomation-dev-ed.lightning.force.com/lightning/o/thn__Item__c/list?filterName=Recent");
+        driver.navigate().to("https://thynk-test-unlocked-dev-ed.lightning.force.com/lightning/o/thn__Item__c/list?filterName=Recent");
+        //driver.navigate().to("https://thautomation-dev-ed.lightning.force.com/lightning/o/thn__Item__c/list?filterName=Recent");
         try{if(wait2.until(ExpectedConditions.alertIsPresent())!=null){
             Alert alert = wait2.until(alertIsPresent());
             alert.accept();
