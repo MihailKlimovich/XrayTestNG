c36c4ee8c783117de659f63a15a7f3722f453c35
Rostislav <rostislav.orel@succraft.com>
2022-05-25 16:38:30 +0300

    AWS - 2

--- src/main/java/pages/LoginPageForPackageOrg.java
+++ src/main/java/pages/LoginPageForPackageOrg.java
@@ -114,7 +114,7 @@ public class LoginPageForPackageOrg extends BasePage{
 
         public void authoriseURL(String sfdxPath, String authURL, String userName) throws IOException, InterruptedException {
             System.out.println("I try!!!!!!!!!!!!!!!!!!!!!!");
-            File createFile = new File("/home/user/SFDX_URL.txt");
+            File createFile = new File("TemporaryFiles");
             createFile.createNewFile();
             PrintWriter pw = new PrintWriter(createFile);
             pw.write(authURL);
@@ -124,14 +124,14 @@ public class LoginPageForPackageOrg extends BasePage{
                     sfdxPath,
                     "auth:sfdxurl:store",
                     "-f",
-                    "/home/user/SFDX_URL.txt",
+                    "TemporaryFiles/SFDX_URL.txt",
                     "-a",
                     userName
             });
             System.out.println(authorise);
             if(createFile.delete()){
-                System.out.println("/home/user/SFDX_URL.txt file deleted");
-            }else System.out.println("/home/user/SFDX_URL.txt file not found");
+                System.out.println("TemporaryFiles/SFDX_URL.txt file deleted");
+            }else System.out.println("TemporaryFiles/SFDX_URL.txt file not found");
         }
 
 
