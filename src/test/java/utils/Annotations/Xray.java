f5885c619764bf5ac79d8a8577f4de9b97f45da3
Rostislav <rostislav.orel@succraft.com>
2022-06-01 15:56:19 +0300

    Xray-5

--- src/test/java/utils/Annotations/Xray.java
+++ src/test/java/utils/Annotations/Xray.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS.RUNTIME)
-public @interface Xray {
+public @interface Xray{
 
     String requirement() default "";
 
