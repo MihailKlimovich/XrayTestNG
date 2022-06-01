744abab12a7b737bcccd233bf234500014288730
Rostislav <rostislav.orel@succraft.com>
2022-06-01 11:24:30 +0300

    Xray-2

---
+++ src/test/java/utils/Annotations/Xray.java
@@ -0,0 +1,17 @@
+package utils.Annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS.RUNTIME)
+public @interface Xray {
+
+    String requirement() default "";
+
+    String test() default "";
+
+    String labels() default "";
+
+}
